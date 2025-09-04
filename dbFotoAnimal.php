da02728245c8842368df9db7905df0e1b2f0086f
profAlbertoAyres <alberto.benicio@ifro.edu.br>
2025-09-04 11:33:01 -0400

    Atualizar

--- dbFotoAnimal.php
+++ dbFotoAnimal.php
@@ -8,12 +8,28 @@ spl_autoload_register(function ($class) {
 $foto = new FotoAnimal();
 $imgFile = new Imagem(prefixo: "anim_");
 if (filter_has_var(INPUT_POST, "btnGravar")):
+    $foto->iniciarTransacao();
+    try {   
+        $idFoto = filter_input(INPUT_POST, 'idFoto');
         $idAnimal = filter_input(INPUT_POST, 'idAnimal');
         $foto->setAnimal( $idAnimal);
+        $nomeArquivo = $imgFile->upload($_FILES['foto']);
         $foto->setNome($nomeArquivo);
         $foto->setAlternativo(filter_input(INPUT_POST, 'textoAlt'));
         $foto->setLegenda(filter_input(INPUT_POST, 'legenda'));
-        $idFoto = filter_input(INPUT_POST, 'idFoto');
+        if(empty($idFoto)){
+            if($foto->add()){
+                $mensagem = 'Foto adicionada com sucesso.';
+            }
+        }
+        $foto->confirmarTransacao();
+        echo "<script>window.alert('$mensagem'); window.location.href='fotoAnimal.php?idAnimal=$idAnimal';</script>";
+    } catch (\Throwable $th) {
+        $foto->cancelarTransacao();
+        $imgFile->deletar($nomeArquivo);
+        $erro = $th->getMessage();
+        echo "<script>window.alert('Erro: $erro.'); window.open(document.referrer,'_self');</script>";
+    }
 
 elseif (filter_has_var(INPUT_POST, "btnDeletar")):
     
--- gerFotoAnimal.php
+++ gerFotoAnimal.php
@@ -11,19 +11,26 @@
 
 <body>
     <header>
-        <?php require_once "_parts/_header.php"; ?>
+        <?php require_once "_parts/_menu.php"; ?>
     </header>
     <main class="container">
         <div class="titutlo mt-3">
-            <h3>Adicionar </h3>
+            <h3>Adicionar</h3>
         </div>
         <?php
         spl_autoload_register(function ($class) {
             require_once "classes/{$class}.class.php";
         });
+        $idAnimal = null;
+        if (filter_has_var(INPUT_POST, 'btnEditar')):
+            //implementar depois
+        elseif (filter_has_var(INPUT_GET, 'idAnimal')):
+            $idAnimal = filter_input(INPUT_GET,'idAnimal');
+        endif;
+
         ?>
         <form method="post" action="dbFotoAnimal.php" class="row g-3 mt-3" enctype="multipart/form-data">
-            <input type="hidden" name="idAnimal" value="">
+            <input type="hidden" name="idAnimal" value="<?php echo $idAnimal ?? null ?>">
             <input type="hidden" name="idFoto" value="">
             <div class="col-md-6 mt-3">
                 <label for="legenda" class="form-label">Legenda</label>
@@ -32,8 +39,8 @@
             </div>
             <div class="col-md-6 mt-3">
                 <label for="textoAlt" class="form-label">Texto alternativo</label>
-                <input type="text" name="textoAlt" id="textoAlt" placehoader="Digite o texto alternativo da foto" required
-                    class="form-control" value="">
+                <input type="text" name="textoAlt" id="textoAlt" placehoader="Digite o texto alternativo da foto"
+                    required class="form-control" value="">
             </div>
             <div class="col-12 mt-3">
                 <label for="foto" class="form-label">Foto</label>
